66bbc772e6501254551b7c873c37768852167c4b
Mary Dick <maryclairedick@gmail.com>
2020-08-15 20:23:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ def base_hash
   hash = {
     :railroads => {}
   }
-
+  hash
 end
 
 def monopoly_with_second_tier
@@ -12,6 +12,7 @@ def monopoly_with_second_tier
     :railroads => {
       :pieces => 4
       :rent_in_dollars => {}
+      :names => {}
     }
   }
 
